8a62d903ad73a1253e5a0c09f27cbe6af305f42a
BabaInformatiker <BabaInformatiker@gmail.com>
2016-12-15 20:21:34 +0100

    Update DynArray.h

--- DynArray.h
+++ DynArray.h
@@ -1,29 +1,26 @@
 #pragma once
-
+#include "Lied.h"
 
 class DynArray
 {
 private:
 	void resize(int newCapacity);
-	DynArray(const DynArray& other); //Kopierkonstruktor soll explizit *nicht* aufgerufen werden
+	DynArray(const DynArray& other); 
 	Lied* m_data;
 	int m_size;
 	int m_capacity;
 
 public:
 	DynArray();
-	DynArray(int newCapacity); //Weiterer Konstruktor
-	~DynArray(); //Destruktor, muss implementiert werden
-
+	DynArray(int newCapacity); 
+	~DynArray(); 
 	Lied& at(int index);
-
 	void push_back(Lied elem);
 	void pop_back();
 	void erase(int index);
-
 	int size();
 	int capacity();
-
 	void print();
-
+        void print(int index);
+        void edit(int index, Lied elem);
 };
