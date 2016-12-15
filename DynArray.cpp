afd12b43fef3c5061b7e9c747a81fbdfd18113d6
BabaInformatiker <BabaInformatiker@gmail.com>
2016-12-15 20:20:59 +0100

    Update DynArray.cpp

--- DynArray.cpp
+++ DynArray.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "DynArray.h"
-#include <iostream>
+#include "Lied.h"
+#include <string>
+
 
 using namespace std;
 
@@ -40,8 +42,23 @@ int DynArray::capacity() {
 
 void DynArray::print() {
 	for (int i = 0; i < m_size; i++) {
-		cout << m_data[i].zahl << endl;
-	}
+		cout << i+1 << ". Eintrag" <<endl;
+                cout << "Titel: " << m_data[i].titel << endl;
+                cout << "Interpret: " <<m_data[i].interpret << endl;
+                cout << "Erscheinungsjahr: " << m_data[i].erscheinungsjahr << endl;
+                cout << "Laenge: " << m_data[i].laenge << endl;
+                //cout << "Genre: " << m_data[i].g << endl;
+                if (m_data[i].g == 0)
+                    cout << "Genre: Pop" << endl;
+                if (m_data[i].g == 1)
+                    cout << "Genre: Rock" << endl;
+                if (m_data[i].g == 2)
+                    cout << "Genre: Klassik" << endl;
+                if (m_data[i].g == 3)
+                    cout << "Genre: Hard Rock" << endl;
+                cout << endl;
+        }
+        
 }
 
 
@@ -61,9 +78,28 @@ void DynArray::push_back(Lied elem) {
         m_capacity = 1;
     if (m_size == m_capacity)
 		resize(m_capacity + m_size);
-
-	m_data[m_size] = elem;
-        m_size += 1;
+    
+   
+    cout << "Bitte den Titel eingeben: ";
+    cin >> elem.titel;
+    cout << "Bitte den Interpreten eingeben: ";
+    cin >> elem.interpret;
+    cout << "Bitte das Erscheinungsjahr eingeben: ";
+    cin >> elem.erscheinungsjahr;
+    cout << "Bitte die Laenge eingeben: ";
+    cin >> elem.laenge;
+    cout << "Bitte das Genre angeben: ";
+    string k;
+        cin >> k;
+        if (k == "pop") { elem.g = pop; }
+        if (k == "rock") { elem.g = rock; }
+        if (k == "klassik") { elem.g = klassik; }
+        if (k == "hard rock") { elem.g = hard_rock; }
+ 
+    
+    m_data[m_size] = elem;
+    m_size += 1;
+    
 }
 
 void DynArray::pop_back() {
@@ -78,7 +114,9 @@ void DynArray::pop_back() {
 }
 
 void DynArray::erase(int index) {
-	Lied* copy = new Lied[m_size - 1];
+        index -= 1;
+        
+        Lied* copy = new Lied[m_size - 1];
 
 	for (int i = 0; i < m_size; i++) {
             if (i < index)
@@ -96,6 +134,74 @@ void DynArray::erase(int index) {
 	
 }
 
+
 /* Lied& DynArray::at(int index){
     return double& m_data[index]; 
 } */
+
+
+void DynArray::print(int index){
+		cout << index << ". Eintrag" <<endl;
+                index -= 1;
+                cout << "Titel: " << m_data[index].titel << endl;
+                cout << "Interpret: " <<m_data[index].interpret << endl;
+                cout << "Erscheinungsjahr: " << m_data[index].erscheinungsjahr << endl;
+                cout << "Laenge: " << m_data[index].laenge << endl;
+                if (m_data[index].g == 0)
+                    cout << "Genre: Pop" << endl;
+                if (m_data[index].g == 1)
+                    cout << "Genre: Rock" << endl;
+                if (m_data[index].g == 2)
+                    cout << "Genre: Klassik" << endl;
+                if (m_data[index].g == 3)
+                    cout << "Genre: Hard Rock" << endl;
+                cout << endl;
+        
+    
+}
+
+void DynArray::edit(int index, Lied elem){
+        string k;
+        string y;
+        int x;
+
+        cout << "Was moechten sie bearbeiten?: ";
+        cin >> y;
+       
+       
+        if (y == "titel" | y == "Titel") {
+            cout << "Bitte den Titel eingeben: ";
+            cin >> k;   
+            m_data[index-1].titel = k;
+        }
+ 
+        if (y == "interpret" | y == "Interpret") {
+            cout << "Bitte den Interpreten eingeben: ";
+            cin >> k;
+            m_data[index-1].interpret = k;
+        }
+ 
+        if (y == "erscheinungsjahr") {
+            cout << "Bitte das Erscheingungsjahr eingeben: ";
+            cin >> x;  
+            m_data[index-1].erscheinungsjahr = x;
+        }
+ 
+        if (y == "laenge") {
+            cout << "Bitte die Laenge des Liedes angeben: ";
+            cin >> x;
+            m_data[index-1].laenge = x;
+        }
+ 
+        if (y == "genre") {
+            cout << "Bitte geben sie das Genre an: ";
+            cin >> k;
+            if (k == "pop") { m_data[index-1].g = pop; }
+            if (k == "rock") { m_data[index-1].g = rock; }
+            if (k == "klassik") { m_data[index-1].g = klassik; }
+            if (k == "hard rock") { m_data[index-1].g = hard_rock; }
+            
+        }
+    
+    
+}
